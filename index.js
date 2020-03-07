9808ee6a95cd2640d211d54b83a99a5584cbae76
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-03-07 05:36:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function createRock(x) {
    * it to GAME and move it downwards.
    */
 
-
+   GAME.appendChild(rock);
   /**
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
